71bd07afdd99d285cea38fc2d751d4a89d63c4d1
Georgi Komov <69540742+gkomov123@users.noreply.github.com>
2025-09-28 03:53:15 +0300

    v1.0 of rock paper scissors by me

---
+++ rock_paper_scizzors.py
@@ -0,0 +1,52 @@
+from random import choice
+
+# Winning moves dictionary
+winning_moves = {
+    "rock": "scissors",
+    "paper": "rock",
+    "scissors": "paper"
+}
+# Hello prints
+print("------------------------------------------------------------------------------------------"
+      "\nWelcome to my Rock, Paper, Scissors game! "
+"\nIn this simple but fun game you pick rock paper or scissors and face off against the computer!"
+"\n----------------------------------------------------------------------------------------------")
+
+# Counters
+user_wins = 0
+computer_wins = 0
+draws = 0
+
+while True:
+    #Computer choice logic
+    computer_move = choice(list(winning_moves.keys()))
+    # User input
+    user_move = input("\nChoose rock, paper or scissors: ").lower()
+
+    # Invalid move case
+    if user_move not in winning_moves:
+        print("\nInvalid move - please type 'rock', 'paper', 'scissors'")
+        continue
+
+    #Win,Draw and Lose Logic
+    if user_move == computer_move:
+        draws += 1
+        print(f"It's a draw the computer chose: {computer_move}")
+    elif winning_moves[user_move] == computer_move:
+        user_wins += 1
+        print(f"You won the computer chose: {computer_move}")
+    else:
+        computer_wins += 1
+        print(f"You lost the computer chose: {computer_move}")
+             
+    # Keep playing logic
+    keep_playing_input = input("\nDo you want to keep playing? y, n: ").lower()
+    valid_input = ["y", "yes", "ye"]
+    if keep_playing_input in valid_input:
+        continue
+    else:
+        print("\nFINAL SCORE:"
+            f"\nComputer:{computer_wins}"
+            f"\nHuman:{user_wins}"
+            f"\nDraws:{draws}")
+        break
